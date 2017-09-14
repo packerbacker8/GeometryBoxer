3c228dcf4ab837e486bdf03c1164b3f8bc35e788
Chandler Waugh <chandler@gbpackerfans.com>
2017-09-14 17:51:30 -0600

    Changed armman prefab and added to some scripts.

--- Geometry Boxer/Assets/Scripts/PunchScript.cs
+++ Geometry Boxer/Assets/Scripts/PunchScript.cs
@@ -6,25 +6,139 @@ public class PunchScript : MonoBehaviour
 {
 
     public bool useController = true;
+    public Rigidbody leftArm;
+    public Rigidbody rightArm;
+    public float punchForce = 50f;
+
+    /// <summary>
+    /// Private variables for controls in punching and moving arms.
+    /// </summary>
+    private bool controllingLeftArm;
+    private bool controllingRightArm;
+   
+    private float leftArmXAxis;
+    private float leftArmYAxis;
+    private float rightArmXAxis;
+    private float rightArmYAxis;
+    
+    private enum Limbs
+    {
+        leftArm = 0,
+        rightArm
+    };
 
     // Use this for initialization
     void Start()
     {
-
+        controllingLeftArm = false;
+        controllingRightArm = false;
+        leftArmXAxis = 0f;
+        leftArmYAxis = 0f;
+        rightArmXAxis = 0f;
+        rightArmYAxis = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q) && this.transform.name == "LowerLeftArm")
+        if (useController)
         {
-            this.GetComponent<Rigidbody>().AddForce(this.transform.forward * 100f);
+            //Left trigger lets player control left arm independently
+            if (Input.GetAxis("LeftTrigger") > 0)
+            {
+                controllingLeftArm = true;
+                this.transform.gameObject.SendMessage("UsingLeftArm", controllingLeftArm);
+            }
+            else
+            {
+                controllingLeftArm = false;
+                this.transform.gameObject.SendMessage("UsingLeftArm", controllingLeftArm);
+            }
+            //Right trigger lets player control right arm independently
+            if (Input.GetAxis("RightTrigger") > 0)
+            {
+                controllingRightArm = true;
+            }
+            else
+            {
+                controllingRightArm = false;
+            }
+            //Left arm punching
+            if (controllingLeftArm)
+            {
+                leftArmXAxis = Input.GetAxisRaw("HorizontalLeft");
+                leftArmYAxis = Input.GetAxisRaw("VerticalLeft");
+                MoveArm(Limbs.leftArm, leftArmXAxis, leftArmYAxis);
+            }
+            else if (Input.GetButtonDown("LeftBumper")) //left bumper
+            {
+                ThrowSinglePunch(Limbs.leftArm);
+            }
+            //Right arm punching
+            if (controllingRightArm)
+            {
+                rightArmXAxis = Input.GetAxisRaw("HorizontalRight");
+                rightArmYAxis = Input.GetAxisRaw("VerticalRight");
+                MoveArm(Limbs.rightArm, rightArmXAxis, rightArmYAxis);
+            }
+            else if (Input.GetButtonDown("RightBumper"))
+            {
+                ThrowSinglePunch(Limbs.rightArm);
+            }
 
         }
-        if (Input.GetKeyDown(KeyCode.E) && this.transform.name == "LowerRightArm")
+        else
         {
-            this.GetComponent<Rigidbody>().AddForce(this.transform.forward * 10000f);
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                ThrowSinglePunch(Limbs.leftArm);
 
+            }
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                ThrowSinglePunch(Limbs.rightArm);
+
+            }
         }
+
+    }
+
+    /// <summary>
+    /// Cause player to throw single punch of one arm.  Arm is determined by limb parameter.
+    /// </summary>
+    /// <param name="limb">The value 0 corresponds to left arm, 1 to right arm.</param>
+    private void ThrowSinglePunch(Limbs limb)
+    {
+        Rigidbody armToMove = null;
+        if(limb == Limbs.leftArm)
+        {
+            armToMove = leftArm;
+        }
+        else if(limb == Limbs.rightArm)
+        {
+            armToMove = rightArm;
+        }
+        armToMove.AddForce((this.transform.forward + this.transform.up) * punchForce, ForceMode.Impulse);
+    }
+
+    /// <summary>
+    /// Allows player to move limb about as they choose with the controller joysticks.
+    /// </summary>
+    /// <param name="limb">The value 0 corresponds to left arm, 1 to right arm.</param>
+    /// <param name="xMotion">Gets axis value from input for left and right sticks. In horizontal direction.</param>
+    /// <param name="yMotion">Gets axis value from input for left and right sticks. In vertical direction.</param>
+    private void MoveArm(Limbs limb, float xMotion, float yMotion)
+    {
+        Rigidbody armToMove = null;
+        if (limb == Limbs.leftArm)
+        {
+            armToMove = leftArm;
+        }
+        else if (limb == Limbs.rightArm)
+        {
+            armToMove = rightArm;
+        }
+        Vector3 directionToMove = new Vector3(xMotion, yMotion);
+        armToMove.AddForce(directionToMove * punchForce * 10f);
     }
 }
--- Geometry Boxer/Assets/Scripts/ThirdPersonCharacterController.cs
+++ Geometry Boxer/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonCharacterController : MonoBehaviour {
+public class ThirdPersonCharacterController : MonoBehaviour
+{
 
     public float speed = 10f;
     public float turnSpeed = 1f;
     public float jumpHeight = 10f;
+    public bool useController = false;
 
     private bool isSprinting = false;
 
@@ -17,44 +19,93 @@ public class ThirdPersonCharacterController : MonoBehaviour {
     public string left = "a";
     public string right = "d";
 
-	// Use this for initialization
-	void Start () {
-       
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
-        if(Input.GetKeyDown(sprint) && !isSprinting)
+    private bool leftStickInUse = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(useController)
         {
-            speed = speed * 2f;
-            isSprinting = true;
+            if(Input.GetButtonDown("LeftStickButton") && !isSprinting)
+            {
+                speed = speed * 2f;
+                isSprinting = true;
+            }
+            if(Input.GetButtonUp("LeftStickButton"))
+            {
+                isSprinting = false;
+                speed = speed / 2f;
+            }
+            if(!leftStickInUse)
+            {
+                if (Input.GetAxisRaw("VerticalLeft") > 0)
+                {
+                    this.transform.position += this.transform.forward * speed * Time.deltaTime;
+                }
+                if (Input.GetAxisRaw("VerticalLeft") < 0)
+                {
+                    this.transform.position -= this.transform.forward * speed * Time.deltaTime;
+                }
+                if (Input.GetAxisRaw("HorizontalLeft") > 0)
+                {
+                    this.transform.Rotate(Vector3.up * turnSpeed);
+                }
+                if (Input.GetAxisRaw("HorizontalLeft") < 0)
+                {
+                    this.transform.Rotate(-Vector3.up * turnSpeed);
+                }
+            }
+            
+            if (Input.GetButton("Jump"))
+            {
+                this.transform.position += this.transform.up * jumpHeight * Time.deltaTime;
+            }
         }
-        if(Input.GetKeyUp(sprint))
+        else
         {
-            isSprinting = false;
-            speed = speed / 2f;
-        }
-        if (Input.GetKey(forward)) 
-        {
-            this.transform.position += this.transform.forward * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(backward))
-        {
-            this.transform.position -= this.transform.forward * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(right))
-        {
-            this.transform.Rotate(Vector3.up * turnSpeed);
-        }
-        if (Input.GetKey(left))
-        {
-            this.transform.Rotate(-Vector3.up * turnSpeed);
-        }
-        if (Input.GetKey(jump))
-        {
-            this.transform.position += this.transform.up * jumpHeight * Time.deltaTime;
+            if (Input.GetKeyDown(sprint) && !isSprinting)
+            {
+                speed = speed * 2f;
+                isSprinting = true;
+            }
+            if (Input.GetKeyUp(sprint))
+            {
+                isSprinting = false;
+                speed = speed / 2f;
+            }
+            if (Input.GetKey(forward))
+            {
+                this.transform.position += this.transform.forward * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(backward))
+            {
+                this.transform.position -= this.transform.forward * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(right))
+            {
+                this.transform.Rotate(Vector3.up * turnSpeed);
+            }
+            if (Input.GetKey(left))
+            {
+                this.transform.Rotate(-Vector3.up * turnSpeed);
+            }
+            if (Input.GetKey(jump))
+            {
+                this.transform.position += this.transform.up * jumpHeight * Time.deltaTime;
+            }
         }
 
+    }
+
+    public void UsingLeftArm(bool inUse)
+    {
+        leftStickInUse = inUse;
     }
 }
