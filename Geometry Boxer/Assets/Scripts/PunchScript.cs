5086e11e4d8864d34d57bb0df81dd832d90443fa
Chandler Waugh <chandler@gbpackerfans.com>
2017-09-14 15:26:27 -0600

    Working more on punch script

--- Geometry Boxer/Assets/Scripts/PunchScript.cs
+++ Geometry Boxer/Assets/Scripts/PunchScript.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PunchScript : MonoBehaviour {
+public class PunchScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+    public bool useController = true;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && this.transform.name == "LowerLeftArm")
+        {
+            this.GetComponent<Rigidbody>().AddForce(this.transform.forward * 100f);
+
+        }
+        if (Input.GetKeyDown(KeyCode.E) && this.transform.name == "LowerRightArm")
         {
             this.GetComponent<Rigidbody>().AddForce(this.transform.forward * 100f);
 
