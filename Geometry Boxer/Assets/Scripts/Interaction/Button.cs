de39e6f45f7740824515e63288ea7b962aa04e67
Henry Kucab <kucab2345@gmail.com>
2017-11-06 21:32:23 -0700

    Beginning interactive test environment

---
+++ Geometry Boxer/Assets/Scripts/Interaction/Button.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+    
+    public List<GameObject> targets;
+    private float activateThreshold = 10f;
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.impulse.magnitude > activateThreshold)
+        {
+            for(int i = 0; i < targets.Count; i++)
+            {
+                targets[i].SendMessage("Activate");
+            }
+            Debug.Log("Button punched");
+        }
+    }
+
+}
