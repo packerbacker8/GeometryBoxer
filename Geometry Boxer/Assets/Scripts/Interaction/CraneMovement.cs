45723df430ca87e59de59c34642df2be85768e73
Henry Kucab <kucab2345@gmail.com>
2017-11-06 22:25:46 -0700

    Made crane oscillate and box drop works

---
+++ Geometry Boxer/Assets/Scripts/Interaction/CraneMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraneMovement : MonoBehaviour
+{
+    public float travelDistance = 10f;
+    Vector3 pointA;
+    Vector3 pointB;
+
+    void Start()
+    {
+        pointA = transform.position;
+        pointB = new Vector3(transform.position.x,transform.position.y,transform.position.z + travelDistance);
+    }
+    void Update()
+    {
+        transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time/2, 1));
+    }
+}
