4bfcb515d19975ff6507893c53a73e100608d5bd
Chandler Waugh <chandler@gbpackerfans.com>
2017-11-09 01:34:33 -0700

    Started looking at how to save and load game data. Might need to use singleton design pattern for saving and loading data. Will need to decide how to write file paths. Is only a start.

--- Geometry Boxer/Assets/Scripts/Game Controlling/SaveAndLoadGame.cs
+++ Geometry Boxer/Assets/Scripts/Game Controlling/SaveAndLoadGame.cs
@@ -1,15 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class SaveAndLoadGame : MonoBehaviour
 {
+    public static SaveAndLoadGame saver;
     private LoadLevel loader;
     private bool hasSavedGame;
 
+    //use player information and store it in a singleton game object
+
     // Use this for initialization
-    void Start()
+    void Awake()
     {
+        if(saver == null)
+        {
+            //might want to not destroy on load
+            saver = this;
+        }
+        else if(saver != this)
+        {
+            //may want to destroy to avoid duplicates
+        }
         loader = this.GetComponent<LoadLevel>();
 
     }
@@ -29,6 +44,25 @@ public class SaveAndLoadGame : MonoBehaviour
     /// <returns>Returns true if success in saving, false otherwise.</returns>
     public bool SaveGame()
     {
+        BinaryFormatter binaryForm = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/saveGame_" + DateTime.Today + ".dat", FileMode.Open);
+
+        GameData data = new GameData();
+
+        binaryForm.Serialize(file, data);
+        file.Close();
+        return true;
+    }
+    /// <summary>
+    /// Function to save game and the player's play session
+    /// information.
+    /// </summary>
+    /// <param name="saveFileName">String that says where to write this file.</param>
+    /// <returns>Returns true if success in saving, false otherwise.</returns>
+    public bool SaveGame(string saveFileName)
+    {
+        BinaryFormatter binaryForm = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/" + saveFileName + ".dat", FileMode.OpenOrCreate);
         return true;
     }
 
@@ -37,6 +71,23 @@ public class SaveAndLoadGame : MonoBehaviour
     /// </summary>
     public void LoadGame()
     {
+
+    }
+
+    /// <summary>
+    /// Function to load game state information.
+    /// </summary>
+    public void LoadGame(string loadFileName)
+    {
+        if (File.Exists(Application.persistentDataPath + "/" + loadFileName + ".dat" ))
+        {
+            BinaryFormatter binaryForm = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/" + loadFileName + ".dat", FileMode.Open);
+            GameData data = (GameData)binaryForm.Deserialize(file);
+            file.Close();
+
+            //set values from data received
+        }
 
     }
 
@@ -60,5 +111,11 @@ public class SaveAndLoadGame : MonoBehaviour
         loader.LoadALevel("CitySelectMap");
     }
 
+    [Serializable]
+    private class GameData
+    {
 
+    }
 }
+
+
