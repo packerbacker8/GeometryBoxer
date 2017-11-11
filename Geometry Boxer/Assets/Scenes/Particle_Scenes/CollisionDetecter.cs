75b4fc25e672977bb88cb10c48301471321b5b54
Grandbuilder <christopherreay@Christophers-MacBook-Pro.local>
2017-11-11 15:02:47 -0700

    Added Particles that show damage. More polish needed.

---
+++ Geometry Boxer/Assets/Scenes/Particle_Scenes/CollisionDetecter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetecter : MonoBehaviour {
+
+	public Rigidbody Character;
+	public Rigidbody Enemy;
+	public Canvas DamageUI;
+
+//	void Start(){
+//		if( DamageUI != null )
+//		{
+//			var myScriptReference = DamageUI.GetComponent<FloatingText>();
+//			if( myScriptReference != null )
+//			{
+//				myScriptReference.SetText(Damage);
+//			}
+//		}
+//	}
+
+	void OnCollisionEnter (Collision Col)
+	{
+		if (Col.gameObject == Enemy ) {
+			Instantiate (DamageUI);
+			Destroy (DamageUI, 1);
+		}
+	}
+}
