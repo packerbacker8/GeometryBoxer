1a94de260ab8485628877b466d7e3bdfd5b4f409
LarryNUofU <lepyzoom@live.com>
2017-11-26 13:43:00 -0700

    New folder in Scenes\Tests folder : Octahedron scripts, temp Punch Script
    Inside LarryNguyenTests folder:
    punchScriptTemp.cs - Almost the same as regular punch script, but doesn't allow the player to quit animations entirely. I added a way to "queue" up an action so the punches can feel smoother.
    OctahedronPunchScript.cs - not sure what it's there for
    OctahedronSpecials.cs - Octahedron can speed up special ability is only one I've really done. I might add more if I know what to do.
    OctahedronStats.cs - not currently used
    Nothing is final, but I felt I had to push something up. The main character that works with these scripts is the octahedron character in CubeShippingYard-octahedronTests. I assume the leads like Henry or Chandler can add my changes to the "main" scripts if they want to.

---
+++ Geometry Boxer/Assets/Scenes/Tests/LarryNguyenTests/OctahedronStats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OctahedronStats : PlayerStatsBaseClass {
+
+	public OctahedronStats() : base(1000f, 1.0f, 1.0f, 1.0f, 1.0f)
+	{
+
+	}
+
+
+	public override void SetPlayerStability (float newStability)
+	{
+		base.SetPlayerStability (newStability);
+
+	}
+
+	public override void SetPlayerSpeed (float newSpeed)
+	{
+		base.SetPlayerSpeed (newSpeed);
+
+	}
+
+
+
+
+
+}
