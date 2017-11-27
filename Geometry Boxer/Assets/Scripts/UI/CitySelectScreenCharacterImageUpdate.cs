7fdb0571c106463812fd3366c2e1352da2479d26
Henry Kucab <kucab2345@gmail.com>
2017-11-27 09:50:09 -0700

    City select screen canvas image now reflects current player selected

---
+++ Geometry Boxer/Assets/Scripts/UI/CitySelectScreenCharacterImageUpdate.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CitySelectScreenCharacterImageUpdate : MonoBehaviour {
+
+    public GameObject[] playerOptions;
+    public GameObject playerImage;
+
+    public Sprite cube;
+    public Sprite sphere;
+    public Sprite octahedron;
+    
+    private Image image;
+    private GameObject activePlayer;
+
+    private string cubeName = "Cubeman";
+    private string sphereName = "Sphere";
+    private string octaName = "Octahedron";
+
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < playerOptions.Length; i++)
+        {
+            if (playerOptions[i].name.Contains(SaveAndLoadGame.saver.GetCharacterType()))
+            {
+                activePlayer = playerOptions[i];
+            }
+        }
+
+        Debug.Log("Active Player Name: " + activePlayer.name.ToString());
+
+        image = playerImage.GetComponent<Image>();
+
+        if(activePlayer.name.Contains(cubeName))
+        {
+            image.sprite = cube;
+        }
+        else if (activePlayer.name.Contains(sphereName))
+        {
+            image.sprite = sphere;
+        }
+        else if (activePlayer.name.Contains(octaName))
+        {
+            image.sprite = octahedron;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
