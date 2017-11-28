d188334e7a44c17634968d3935206932f0b2ccfd
Chandler Waugh <chandler@gbpackerfans.com>
2017-11-28 00:18:01 -0700

    Will have to determine correct way to obtain enemy beahviour puppet and rigidbody to launch them in the air.

--- Geometry Boxer/Assets/Scripts/Interaction/FireHydrantForce.cs
+++ Geometry Boxer/Assets/Scripts/Interaction/FireHydrantForce.cs
@@ -5,15 +5,21 @@ using RootMotion.Dynamics;
 
 public class FireHydrantForce : MonoBehaviour
 {
-
     private void OnTriggerEnter(Collider other)
     {
         if(other.transform.tag.Contains("Player") || other.transform.tag.Contains("Enemy"))
         {
             //other.transform.root.GetComponentInChildren<PuppetMaster>().pinWeight = 0;
             //other.transform.root.GetComponentInChildren<PuppetMaster>().muscleWeight = 0;
-            other.transform.root.GetComponentInChildren<BehaviourPuppet>().SetState(BehaviourPuppet.State.Unpinned);
-            other.transform.GetComponentInChildren<Rigidbody>().AddForce(Vector3.up * 100);
+            if(other.transform.root.GetComponentInChildren<BehaviourPuppet>() == null)
+            {
+                other.transform.GetComponentInChildren<Rigidbody>().AddForce(Vector3.up * 100);
+            }
+            else
+            {
+                other.transform.root.GetComponentInChildren<BehaviourPuppet>().SetState(BehaviourPuppet.State.Unpinned);
+                other.transform.GetComponentInChildren<Rigidbody>().AddForce(Vector3.up * 100);
+            }
         }
 
     }
@@ -31,7 +37,6 @@ public class FireHydrantForce : MonoBehaviour
         if (other.transform.tag.Contains("Player") || other.transform.tag.Contains("Enemy"))
         {
             other.transform.GetComponentInChildren<Rigidbody>().AddForce(Vector3.up * 100);
-            //other.transform.root.GetComponentInChildren<PuppetMaster>().muscleWeight = 1;
         }
     }
 }
