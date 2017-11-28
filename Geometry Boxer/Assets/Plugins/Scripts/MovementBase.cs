ea9f4a228acec6c0ec491f8927dbf8c22180ecf2
Ryan Joseph Steele <ryan.steele.j@gmail.com>
2017-11-27 19:08:50 -0700

    Created start of movementBase and AttackBase for AI

---
+++ Geometry Boxer/Assets/Plugins/Scripts/MovementBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RootMotion.Demos;
+
+using UnityEngine.AI;
+namespace Enemy
+{
+    public interface MovementBase
+    {
+        void move();
+
+        bool canMove();
+
+        void setUp(float stopDist, float stopThresh, float jumpDis, Animator animator,
+            Transform move, UserControlThirdPerson.State baseState, NavMeshAgent baseAgent);
+    }
+}
