fe2fba0be1222426db321b6635b35417a3126310
Chandler Waugh <chandler@gbpackerfans.com>
2017-11-28 00:10:36 -0700

    Added force to fire hydrant water. Doesn't affect enemies as of now for some reason.

--- Geometry Boxer/Assets/Scripts/Interaction/FireHydrantForce.cs
+++ Geometry Boxer/Assets/Scripts/Interaction/FireHydrantForce.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RootMotion.Dynamics;
 
-public class FireHydrantForce : MonoBehaviour {
+public class FireHydrantForce : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag.Contains("Player") || other.transform.tag.Contains("Enemy"))
+        {
+            //other.transform.root.GetComponentInChildren<PuppetMaster>().pinWeight = 0;
+            //other.transform.root.GetComponentInChildren<PuppetMaster>().muscleWeight = 0;
+            other.transform.root.GetComponentInChildren<BehaviourPuppet>().SetState(BehaviourPuppet.State.Unpinned);
+            other.transform.GetComponentInChildren<Rigidbody>().AddForce(Vector3.up * 100);
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.transform.tag.Contains("Player")|| other.transform.tag.Contains("Enemy"))
+        {
+            other.transform.GetComponentInChildren<Rigidbody>().AddForce(Vector3.up * 100);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag.Contains("Player") || other.transform.tag.Contains("Enemy"))
+        {
+            other.transform.GetComponentInChildren<Rigidbody>().AddForce(Vector3.up * 100);
+            //other.transform.root.GetComponentInChildren<PuppetMaster>().muscleWeight = 1;
+        }
+    }
 }
