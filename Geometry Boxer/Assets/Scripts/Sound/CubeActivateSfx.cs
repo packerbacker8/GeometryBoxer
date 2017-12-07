87ec752c8ccaaf23659477fcce32be60b8f0213b
Henry Kucab <kucab2345@gmail.com>
2017-12-07 16:46:18 -0700

    Adding enemies to all maps

---
+++ Geometry Boxer/Assets/Scripts/Sound/CubeActivateSfx.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeActivateSfx : MonoBehaviour {
+
+    public AudioClip activate;
+    public AudioClip deactivate;
+    private AudioSource source;
+    // Use this for initialization
+    void Start()
+    {
+        source = gameObject.AddComponent<AudioSource>();
+    }
+
+    void CubeActivatedSfx()
+    {
+        source.PlayOneShot(activate, 1f);
+    }
+    void CubeDeactivatedSfx()
+    {
+        source.PlayOneShot(deactivate, 1f);
+    }
+}
