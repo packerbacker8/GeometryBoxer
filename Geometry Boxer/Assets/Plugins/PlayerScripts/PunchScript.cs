258e7bcd53f77275717ea8ec90622e39b3ba6cdd
Chandler Waugh <chandler@gbpackerfans.com>
2017-12-19 23:40:33 -0700

    Starting to try to add bigger colliders in front of punches and kicks to hit more area. Problem though with fists where colliders start below ground so get stuck.  Need a way for them to stay above ground and rotate to correct position during animation.

--- Geometry Boxer/Assets/Plugins/PlayerScripts/PunchScript.cs
+++ Geometry Boxer/Assets/Plugins/PlayerScripts/PunchScript.cs
@@ -7,12 +7,15 @@ using RootMotion.Demos;
 
 public class PunchScript : MonoBehaviour
 {
-    
     [Header("Punching Components")]
-    [Tooltip("Arm designated as the left arm.  This will move it by its rigidbody.")]
-    public Rigidbody leftArm;
-    [Tooltip("Arm designated as the right arm.  This will move it by its rigidbody.")]
-    public Rigidbody rightArm;
+    [Tooltip("Object designated as the collider in front of the left fist.")]
+    public Collider leftFistCollider;
+    [Tooltip("Object designated as the collider in front of the right fist.")]
+    public Collider rightFistCollider;
+    [Tooltip("Object designated as the collider in front of the left foot.")]
+    public Collider leftFootCollider;
+    [Tooltip("Object designated as the collider in front of the right foot.")]
+    public Collider rightFootCollider;
     [Tooltip("The force by which the rigidbody will move.")]
     public float punchForce = 50f;
     [Header("PC punch buttons.")]
@@ -46,8 +49,6 @@ public class PunchScript : MonoBehaviour
     /// <summary>
     /// Private variables for controls in punching and moving arms.
     /// </summary>
-    protected bool controllingLeftArm; //controlling left arm or right arm is the same as disabling puppet master control
-    protected bool controllingRightArm;
     protected bool leftGrab;
     protected bool rightGrab;
     protected bool movementAndCameraDisabled;
@@ -64,7 +65,7 @@ public class PunchScript : MonoBehaviour
     protected float oldInputX;
     protected float oldInputY;
     protected float currentAnimLength;
-    
+
     protected Animator anim;
     protected int characterControllerIndex = 2;
     protected int animationControllerIndex = 0;
@@ -75,7 +76,7 @@ public class PunchScript : MonoBehaviour
     protected int cameraIndex = 3;
     protected int behaviorIndex = 0;
     protected int puppetArmBehaviorIndex = 1;
-    
+
     protected string leftPunchAnimation = "Hit";
     protected string rightPunchAnimation = "Hit";
     protected string leftUppercutAnimation = "LeftUpperCut";
@@ -104,15 +105,17 @@ public class PunchScript : MonoBehaviour
     // Use this for initialization
     protected virtual void Start()
     {
-        controllingLeftArm = false;
-        controllingRightArm = false;
+        leftFistCollider.enabled = false;
+        rightFistCollider.enabled = false;
+        leftFootCollider.enabled = false;
+        rightFootCollider.enabled = false;
         leftGrab = false;
         rightGrab = false;
         movementAndCameraDisabled = false;
         useController = false;
         isAttacking = false;
         controllerInfo = Input.GetJoystickNames();
-        if(controllerInfo.Length > 0)
+        if (controllerInfo.Length > 0)
         {
             useController = true;
         }
@@ -135,27 +138,19 @@ public class PunchScript : MonoBehaviour
         puppetMaster = puppetMastObject.GetComponent<PuppetMaster>();
         numberOfMuscleComponents = puppetMastObject.GetComponent<PuppetMaster>().muscles.Length;
         armMuscles = new List<Muscle>();
-//        foreach (Muscle m in puppetMaster.muscles) //NOT RIGHT DOING IT FOR WHOLE BODY NOT JUST ARMS, STILL IN ANIM POSITION
-//        {
-//            if (m.name.Contains("arm") || m.name.Contains("hand"))
-//            {
-//                armMuscles.Add(m);
-//            }
-//        }
-//    }
-			foreach (Muscle m in puppetMaster.muscles) 
-			{
-				if (m.props.group == Muscle.Group.Arm || m.props.group == Muscle.Group.Hand)
-				{
-					armMuscles.Add(m);
-				}
-			}
-	}
+        foreach (Muscle m in puppetMaster.muscles)
+        {
+            if (m.props.group == Muscle.Group.Arm || m.props.group == Muscle.Group.Hand)
+            {
+                armMuscles.Add(m);
+            }
+        }
+    }
     // Update is called once per frame
     protected virtual void Update()
     {
         useController = controllerInfo.Length > 0;
-        
+
         if (numberOfMuscleComponents < puppetMaster.muscles.Length) //number of muscles increased from beginning, a prop has been picked up
         {
             rightGrab = true;
@@ -165,20 +160,28 @@ public class PunchScript : MonoBehaviour
             rightGrab = false;
         }
         AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
-        if(!info.IsName("Hit"))
+        if (!info.IsName("Hit"))
         {
             anim.speed = 1f;
         }
-        if(isAttacking)
+        if (isAttacking)
         {
             currentAnimLength -= Time.deltaTime;
-            if(currentAnimLength <= 0f)
+            if (currentAnimLength <= 0f)
             {
                 isAttacking = false;
+                leftFistCollider.enabled = false;
+                rightFistCollider.enabled = false;
+                leftFootCollider.enabled = false;
+                rightFootCollider.enabled = false;
             }
         }
         else
         {
+            leftFistCollider.enabled = false;
+            rightFistCollider.enabled = false;
+            leftFootCollider.enabled = false;
+            rightFootCollider.enabled = false;
             if (!info.IsName(getUpProne) && !info.IsName(getUpSupine) && !info.IsName(fall) && anim.GetBool(onGround)) //prevent use of your arms when you are on the ground and getting up.
             {
                 if (useController) //controller controls
@@ -186,58 +189,28 @@ public class PunchScript : MonoBehaviour
                     //Left trigger lets player control left arm independently
                     if (Input.GetAxis("LeftTrigger") > 0)
                     {
-                        controllingLeftArm = true;
+
                     }
                     else
                     {
-                        controllingLeftArm = false;
+
                     }
                     //Right trigger lets player control right arm independently
                     if (Input.GetAxis("RightTrigger") > 0)
                     {
-                        controllingRightArm = true;
+                        
                     }
                     else
                     {
-                        controllingRightArm = false;
+                        
                     }
-                    //turn off camera movement and character movement
-                    StartCoroutine(ToggleCameraAndMovement(controllingLeftArm || controllingRightArm));
-                    StartCoroutine(ToggleArmPuppetMaster(controllingLeftArm, Limbs.leftArm));
-                    StartCoroutine(ToggleArmPuppetMaster(controllingRightArm, Limbs.rightArm));
                     //Left arm punching
-                    if (controllingLeftArm)
-                    {
-                        leftArmXAxis = Input.GetAxisRaw("HorizontalLeft");
-                        leftArmYAxis = Input.GetAxisRaw("VerticalLeft");
-                        float xSquare = (leftArmXAxis - oldInputX) * (leftArmXAxis - oldInputX);
-                        float ySquare = (leftArmYAxis - oldInputY) * (leftArmYAxis - oldInputY);
-                        if (System.Math.Sqrt(xSquare + ySquare) > marginOfError)
-                        {
-                            MoveArm(Limbs.leftArm, leftArmXAxis, leftArmYAxis);
-                        }
-                        oldInputX = leftArmXAxis;
-                        oldInputY = leftArmYAxis;
-                    }
-                    else if (Input.GetButtonDown(leftJabControllerButton)) //left bumper
+
+                    if (Input.GetButtonDown(leftJabControllerButton)) //left bumper
                     {
                         ThrowSinglePunch(Limbs.leftArm);
                     }
-                    //Right arm punching
-                    if (controllingRightArm)
-                    {
-                        rightArmXAxis = Input.GetAxisRaw("HorizontalRight");
-                        rightArmYAxis = Input.GetAxisRaw("VerticalRight");
-                        float xSquare = (rightArmXAxis - oldInputX) * (rightArmXAxis - oldInputX);
-                        float ySquare = (rightArmYAxis - oldInputY) * (rightArmYAxis - oldInputY);
-                        if (System.Math.Sqrt(xSquare + ySquare) > marginOfError)
-                        {
-                            MoveArm(Limbs.rightArm, rightArmXAxis, rightArmYAxis);
-                        }
-                        oldInputX = rightArmXAxis;
-                        oldInputY = rightArmYAxis;
-                    }
-                    else if (Input.GetButtonDown(rightJabControllerButton))
+                    if (Input.GetButtonDown(rightJabControllerButton))
                     {
                         ThrowSinglePunch(Limbs.rightArm);
                     }
@@ -248,46 +221,24 @@ public class PunchScript : MonoBehaviour
                     //Left trigger lets player control left arm independently
                     if (Input.GetKeyDown(KeyCode.Mouse0))
                     {
-                        controllingLeftArm = true;
+                        
                     }
                     else if (Input.GetKeyUp(KeyCode.Mouse0))
                     {
-                        controllingLeftArm = false;
+                        
                     }
                     //Right trigger lets player control right arm independently
                     if (Input.GetKeyDown(KeyCode.Mouse1))
                     {
-                        controllingRightArm = true;
+                        
 
                     }
                     else if (Input.GetKeyUp(KeyCode.Mouse1))
                     {
-                        controllingRightArm = false;
+                        
                     }
-                    //turn off camera movement and character movement
-                    StartCoroutine(ToggleCameraAndMovement(controllingLeftArm || controllingRightArm));
-                    StartCoroutine(ToggleArmPuppetMaster(controllingLeftArm, Limbs.leftArm));
-                    StartCoroutine(ToggleArmPuppetMaster(controllingRightArm, Limbs.rightArm));
 
-                    if (controllingLeftArm)
-                    {
-                        leftArmXAxis = Input.GetAxisRaw("Mouse X");
-                        leftArmYAxis = Input.GetAxisRaw("Mouse Y");
-                        //currentX = Input.mousePosition.x;
-                        //currentY = Input.mousePosition.y;
-                        float xSquare = (leftArmXAxis - oldInputX) * (leftArmXAxis - oldInputX);
-                        float ySquare = (leftArmYAxis - oldInputY) * (leftArmYAxis - oldInputY);
-                        if (System.Math.Sqrt(xSquare + ySquare) > marginOfError)
-                        {
-                            //leftArmXAxis = (currentX - halfScreenWidth) / halfScreenWidth;
-                            //leftArmYAxis = (currentY - halfScreenHeight) / halfScreenHeight;
-                            MoveArm(Limbs.leftArm, leftArmXAxis, leftArmYAxis);
-                        }
-
-                        oldInputX = currentX;
-                        oldInputY = currentY;
-                    }
-                    else if (Input.GetKeyDown(leftJabKey))
+                    if (Input.GetKeyDown(leftJabKey))
                     {
                         ThrowSinglePunch(Limbs.leftArm);
                     }
@@ -295,25 +246,8 @@ public class PunchScript : MonoBehaviour
                     {
                         ThrowUppercut(Limbs.leftArm);
                     }
-                    if (controllingRightArm)
-                    {
-                        rightArmXAxis = Input.GetAxisRaw("Mouse X");
-                        rightArmYAxis = Input.GetAxisRaw("Mouse Y");
-                        //currentY = Input.mousePosition.y;
-                        //currentX = Input.mousePosition.x;
-                        float xSquare = (rightArmXAxis - oldInputX) * (rightArmXAxis - oldInputX);
-                        float ySquare = (rightArmYAxis - oldInputY) * (rightArmYAxis - oldInputY);
-                        if (System.Math.Sqrt(xSquare + ySquare) > marginOfError)
-                        {
-                            //rightArmXAxis = (currentX - halfScreenWidth) / halfScreenWidth;
-                            //rightArmYAxis = (currentY - halfScreenHeight) / halfScreenHeight;
-                            MoveArm(Limbs.rightArm, rightArmXAxis, rightArmYAxis);
-                        }
 
-                        oldInputX = currentX;
-                        oldInputY = currentY;
-                    }
-                    else if (Input.GetKeyDown(rightJabKey))
+                    if (Input.GetKeyDown(rightJabKey))
                     {
                         ThrowSinglePunch(Limbs.rightArm);
                     }
@@ -332,7 +266,7 @@ public class PunchScript : MonoBehaviour
                 //do something if down on the ground, ground combat
             }
         }
-        
+
     }
 
     /// <summary>
@@ -346,15 +280,16 @@ public class PunchScript : MonoBehaviour
         {
             if (limb == Limbs.leftArm)
             {
-                if(leftGrab && action.animName == "SwingProp")
+                if (leftGrab && action.animName == "SwingProp")
                 {
                     currentAnim = action;
                     break;
                 }
-                else if(!leftGrab && action.animName == "LeftPunch")
+                else if (!leftGrab && action.animName == "LeftPunch")
                 {
+                    leftFistCollider.enabled = true;
                     currentAnim = action;
-                    if(anim.GetFloat("Forward") < 0.5f)
+                    if (anim.GetFloat("Forward") < 0.5f)
                     {
                         currentAnim.animLayer = 0;
                     }
@@ -363,7 +298,7 @@ public class PunchScript : MonoBehaviour
             }
             if (limb == Limbs.rightArm)
             {
-                
+
                 if (rightGrab && action.animName == "SwingProp")
                 {
                     currentAnim = action;
@@ -371,6 +306,7 @@ public class PunchScript : MonoBehaviour
                 }
                 else if (!rightGrab && action.animName == "RightPunch")
                 {
+                    rightFistCollider.enabled = true;
                     currentAnim = action;
                     //anim.speed = 5f;
                     if (anim.GetFloat("Forward") < 0.5f)
@@ -404,11 +340,13 @@ public class PunchScript : MonoBehaviour
         {
             if (limb == Limbs.leftArm && action.animName == "LeftUpperCut")
             {
+                leftFistCollider.enabled = true;
                 currentAnim = action;
                 break;
             }
             if (limb == Limbs.rightArm && action.animName == "RightUpperCut")
             {
+                rightFistCollider.enabled = true;
                 currentAnim = action;
                 if (anim.GetFloat("Forward") < 0.5f)
                 {
@@ -438,6 +376,7 @@ public class PunchScript : MonoBehaviour
         {
             if (action.animName == "HiKick")
             {
+                rightFootCollider.enabled = true;
                 currentAnim = action;
                 break;
             }
@@ -457,14 +396,13 @@ public class PunchScript : MonoBehaviour
     /// <param name="yMotion">Gets axis value from input for left and right sticks. In vertical direction.</param>
     public virtual void MoveArm(Limbs limb, float xMotion, float yMotion)
     {
+        /*
+         * Currently phased out.
+         * 
         //use camera's forward to determine the forward (z direction) of the movement
         Rigidbody armToMove = null;
         xMotion = System.Math.Abs(xMotion);  //make go the right direction, so arm doesn't go through body
         Vector2 forceOfMove = new Vector2(xMotion, System.Math.Abs(yMotion)); //the force multiplied to the punch
-        //if(useController)  //controller needs a little more force
-        //{
-
-        //}
         float sum = xMotion + System.Math.Abs(yMotion);
         if (sum == 0f)
         {
@@ -487,6 +425,7 @@ public class PunchScript : MonoBehaviour
         }
         Vector3 directionToMove = new Vector3(upMotion, sideMotion, forwardMotion); //x is up, y is to the side, z is forward 
         armToMove.AddForce(directionToMove * forceOfMove.sqrMagnitude, ForceMode.Impulse);
+        */
     }
 
     /// <summary>
