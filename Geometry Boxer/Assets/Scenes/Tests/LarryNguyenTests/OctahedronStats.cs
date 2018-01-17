3d31828563d95d76225d8d15db234fdf3606e0db
Henry Kucab <kucab2345@gmail.com>
2018-01-17 14:46:13 -0700

    Health working on Octahedron. Testing...

--- Geometry Boxer/Assets/Scenes/Tests/LarryNguyenTests/OctahedronStats.cs
+++ Geometry Boxer/Assets/Scenes/Tests/LarryNguyenTests/OctahedronStats.cs
@@ -1,29 +1,90 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using RootMotion.Dynamics;
+using RootMotion.Demos;
+using System;
 
 public class OctahedronStats : PlayerStatsBaseClass {
 
-	public OctahedronStats() : base(1000f, 1.0f, 1.0f, 1.0f, 1.0f)
+    public float Health = 15000f;
+
+    private float originalHealth;
+    private float HealthModifier;
+    private Image healthBarBackground;
+    private Image healthBarFill;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        HealthModifier = 1.0f;
+        health = Health;
+        originalHealth = health;
+        healthBarBackground = GameObject.FindGameObjectWithTag("HealthBarBackground").GetComponent<Image>();
+        healthBarFill = GameObject.FindGameObjectWithTag("HealthBarBackground").transform.GetChild(0).GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    protected override void LateUpdate()
+    {
+        base.LateUpdate();
+        if (health <= 0f && !dead)
+        {
+            dead = true;
+            KillPlayer();
+        }
+    }
+
+    /// <summary>
+    /// Function to kill enemy AI unit, plays associated death animation then removes the object.
+    /// </summary>
+    public override void KillPlayer()
+    {
+        anim.Play("Death");
+        puppetMast.GetComponent<PuppetMaster>().state = PuppetMaster.State.Dead;
+        gameController.GetComponent<GameControllerScript>().playerKilled();
+
+        //Destroy(this.transform.gameObject,deathDelay);  //To be destroyed by game manager if body count exceeds certain amout.
+    }
+
+    public override void PlayerBeingReset(Transform resetLocation)
+    {
+        //base.PlayerBeingReset(resetLocation);
+        LoadLevel.loader.ReloadScene();
+    }
+
+
+    public override void ImpactReceived(Collision collision)
+    {
+        if (collision.gameObject.tag == "EnemyCollision")  //|| (!info.IsName(getUpProne) && !info.IsName(getUpSupine)))
+        {
+            hitByEnemy = true;
+            if (!dead && collision.impulse.magnitude > damageThreshold)
+            {
+                SetPlayerHealth(Math.Abs(collision.impulse.magnitude) / HealthModifier);
+            }
+            UpdateHealthUI();
+        }
+        else if (hitByEnemy)
+        {
+            if (!dead && collision.impulse.magnitude > damageThreshold)
+            {
+                SetPlayerHealth(Math.Abs(collision.impulse.magnitude) / HealthModifier);
+            }
+            UpdateHealthUI();
+        }
+    }
+    private void UpdateHealthUI()
+    {
+        Debug.Log("Health: " + GetPlayerHealth());
+        healthBarFill.fillAmount = GetPlayerHealth() / originalHealth;
+    }
+    /*
+    public OctahedronStats() : base(1000f, 1.0f, 1.0f, 1.0f, 1.0f)
 	{
 
 	}
-
-
-	public override void SetPlayerStability (float newStability)
-	{
-		base.SetPlayerStability (newStability);
-
-	}
-
-	public override void SetPlayerSpeed (float newSpeed)
-	{
-		base.SetPlayerSpeed (newSpeed);
-
-	}
-
-
-
-
-
+    */
 }
