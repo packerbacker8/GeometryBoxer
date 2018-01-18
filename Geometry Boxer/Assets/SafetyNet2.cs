c69d91c78bd9b719f3943f5076b368d34f9aedf6
junok <u0830932@utah.edu>
2018-01-18 13:03:50 -0700

    Implemented a different way for SafetyNet.
    Need to add safety net prefab to all scenes

---
+++ Geometry Boxer/Assets/SafetyNet2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafetyNet2 : MonoBehaviour {
+    public Vector3 respawnPt;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerExit(Collider collision)
+    {
+        Debug.Log("SafetyNet");
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.transform.GetChild(2).gameObject.GetComponent<Transform>().position = respawnPt;
+        }
+    }
+}
