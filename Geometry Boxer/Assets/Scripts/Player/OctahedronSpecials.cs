6d1ffcc85bd22f1458a613381eaaf0c60a9472d7
Chandler Waugh <chandler@gbpackerfans.com>
2018-01-21 18:34:26 -0700

    Replaced square with octahedron model. Needs some tweaking to make work right. Still odd.

--- Geometry Boxer/Assets/Scripts/Player/OctahedronSpecials.cs
+++ Geometry Boxer/Assets/Scripts/Player/OctahedronSpecials.cs
@@ -6,6 +6,8 @@ using RootMotion;
 
 public class OctahedronSpecials : PunchScript
 {
+    public float spinForceMult = 500f;
+
     CharacterMeleeDemo charMeleeDemoRef;
     UserControlThirdPerson userControlThirdPersonRef;
     CharacterThirdPerson charThirdPersonref;
@@ -22,6 +24,7 @@ public class OctahedronSpecials : PunchScript
     private float launchLength;
     private float floatHeight;
     private float floatOffset;
+    private float octahedronExtension;
 
     private bool executingSpecial = false;
     private bool specialIsSpeed = false;
@@ -31,6 +34,8 @@ public class OctahedronSpecials : PunchScript
     private int isFloating;
 
     private OctahedronStats stats;
+
+    private MeshCollider specialFormCollider;
     
 
     // Use this for initialization
@@ -44,13 +49,11 @@ public class OctahedronSpecials : PunchScript
         Rigidbody[] arr = transform.GetComponentsInChildren<Rigidbody>();
         rb = arr[11];
 
-        isGrounded = checkIfGrounded();
-        isFloating = 0;
-
         stats = this.GetComponent<OctahedronStats>();
         baseStats = this.GetComponent<OctahedronStats>();
+        specialFormCollider = specialForm.GetComponent<MeshCollider>();
         specialForm.GetComponent<MeshRenderer>().enabled = false;
-        specialForm.GetComponent<BoxCollider>().enabled = false;   // TEMPORARILY A CUBE COLLIDER UNTIL MESH IS FIXED
+        specialFormCollider.enabled = false;   // TEMPORARILY A CUBE COLLIDER UNTIL MESH IS FIXED
         specialRigid = specialForm.GetComponent<Rigidbody>();
 
         coolDownTimer = 0f;
@@ -60,6 +63,14 @@ public class OctahedronSpecials : PunchScript
         floatHeight = 1.5f;
         floatOffset = 0.2f;
         specialRigid.useGravity = false;
+        specialRigid.constraints = RigidbodyConstraints.FreezeRotationX;
+        specialRigid.constraints = RigidbodyConstraints.FreezeRotationZ;
+        octahedronExtension = 5f;
+        specialStartSize = new Vector3(0.1f * octahedronExtension, 0.1f, 0.1f);
+        specialEndSize = new Vector3(specialFormSize * octahedronExtension, specialFormSize, specialFormSize);
+
+        isGrounded = checkIfGrounded();
+        isFloating = 0;
     }
 
     // Update is called once per frame
@@ -139,7 +150,7 @@ public class OctahedronSpecials : PunchScript
                 moveDir.x = moveDir.x * octaForce * stats.GetPlayerSpeed();
                 moveDir.z = moveDir.z * octaForce * stats.GetPlayerSpeed();
                 specialRigid.AddForce(moveDir);
-                specialRigid.AddTorque(Vector3.up * octaForce * 100f);
+                specialRigid.AddTorque(Vector3.up * octaForce * spinForceMult);
             }
             
         }
@@ -238,11 +249,11 @@ public class OctahedronSpecials : PunchScript
         }
     }
 
-    /*
+    
     protected override bool checkIfGrounded()
     {
-        return Physics.Raycast(specialForm.transform.position, -Vector3.up, specialForm.GetComponent<MeshCollider>().size.y + 0.1f);
-    }*/
+        return Physics.Raycast(specialForm.transform.position, -Vector3.up, specialFormCollider.bounds.size.y + 0.1f);
+    }
 
     /// <summary>
     /// Check to see if the octahedron is floating above the ground within a certain range.
@@ -253,10 +264,10 @@ public class OctahedronSpecials : PunchScript
     private int checkIfFloating()
     {
         int result = 0;
-        Vector3 endPoint = new Vector3(specialForm.transform.position.x, specialForm.transform.position.y - specialForm.GetComponent<BoxCollider>().size.y * 2f , specialForm.transform.position.z);
+        Vector3 endPoint = new Vector3(specialForm.transform.position.x, specialForm.transform.position.y - specialFormCollider.bounds.size.y * 2f , specialForm.transform.position.z);
         Debug.DrawLine(specialForm.transform.position, endPoint, Color.red, 5f);
-        bool lower = Physics.Raycast(specialForm.transform.position, -Vector3.up, specialForm.GetComponent<BoxCollider>().size.y * floatHeight);
-        bool upper = Physics.Raycast(specialForm.transform.position, -Vector3.up, specialForm.GetComponent<BoxCollider>().size.y * (floatHeight + floatOffset));
+        bool lower = Physics.Raycast(specialForm.transform.position, -Vector3.up, specialFormCollider.bounds.size.y * floatHeight);
+        bool upper = Physics.Raycast(specialForm.transform.position, -Vector3.up, specialFormCollider.bounds.size.y * (floatHeight + floatOffset));
         if (lower && upper) //within cast range of both raycasts
         {
             result = -1;
@@ -268,7 +279,7 @@ public class OctahedronSpecials : PunchScript
         return result;
     }
 
-    /*
+    
     /// <summary>
     /// Method that grows the player and shrinks the special form at the same time.
     /// This allows the feeling of changing form.
@@ -283,14 +294,100 @@ public class OctahedronSpecials : PunchScript
         {
             charController.transform.localScale = playerFinalSize;
             specialForm.GetComponent<MeshRenderer>().enabled = false;
-            specialForm.GetComponent<MeshCollider>().enabled = false; //CHANGE TO MESH COLLIDER WHEN OCTAHEDRON BROUGHT IN
+            specialFormCollider.enabled = false; //CHANGE TO MESH COLLIDER WHEN OCTAHEDRON BROUGHT IN
             specialRigid.velocity = Vector3.zero;
             playerGrowing = false;
         }
         if (specialForm.transform.localScale.magnitude <= specialStartSize.magnitude)
         {
             specialForm.transform.localScale = specialStartSize;
-
         }
-    }*/
+    }
+
+    /// <summary>
+    /// Turn off the special attack and reactivate the player character.
+    /// </summary>
+    protected override void DeactivateSpecialAttack()
+    {
+        playerGrowing = true;
+        charController.transform.localScale = playerStartSize;
+        charController.GetComponent<Rigidbody>().velocity = new Vector3(charController.GetComponent<Rigidbody>().velocity.x, 0, charController.GetComponent<Rigidbody>().velocity.z);
+        specialRigid.useGravity = false;
+        launched = false;
+        UpdatePos(charController.transform, specialForm.transform);
+        //play animation of morphing into ball
+        isAttacking = false;
+        for (int i = 0; i < this.transform.childCount; i++) //move camera back to player here
+        {
+            if (this.transform.GetChild(i).gameObject != specialForm)
+            {
+                if (this.transform.GetChild(i).gameObject.tag == "MainCamera") //move camera to follow ball here
+                {
+                    this.transform.GetChild(i).gameObject.GetComponent<CameraController>().target = baseStats.pelvisJoint.transform;
+                }
+                else if (this.transform.GetChild(i).gameObject != charController)
+                {
+                    this.transform.GetChild(i).gameObject.SetActive(true);
+                }
+            }
+        }
+        charController.GetComponent<UserControlMelee>().enabled = true;
+        charController.GetComponent<CharacterMeleeDemo>().enabled = true;
+        charController.GetComponent<CapsuleCollider>().enabled = true;
+        charController.GetComponentInChildren<SkinnedMeshRenderer>().enabled = true;
+        onCooldown = true;
+        anim.SetInteger("ActionIndex", -1);
+        anim.SetBool("IsStrafing", false);
+        if (specialRigid.velocity.sqrMagnitude > 0)
+        {
+            anim.SetFloat("Forward", 1);
+        }
+        else
+        {
+            anim.SetFloat("Forward", 0);
+        }
+        anim.Play("Grounded Directional");
+        SendMessage("PowerUpDeactivated", false, SendMessageOptions.DontRequireReceiver);
+    }
+
+    /// <summary>
+    /// Turn on the special attack and deactivate the player character.
+    /// </summary>
+    protected override void ActivateSpecialAttack()
+    {
+        specialRigid.useGravity = true;
+        leftFistCollider.radius = leftFistStartSize.radius;
+        leftFistCollider.height = leftFistStartSize.height;
+        rightFistCollider.radius = rightFistStartSize.radius;
+        rightFistCollider.height = rightFistStartSize.height;
+        UpdatePos(specialForm.transform, charController.transform);
+        isAttacking = true;
+        //play animation of morphing into ball
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            if (this.transform.GetChild(i).gameObject != specialForm)
+            {
+                if (this.transform.GetChild(i).gameObject.tag == "MainCamera") //move camera to follow ball here
+                {
+                    this.transform.GetChild(i).gameObject.GetComponent<CameraController>().target = specialForm.transform;
+                }
+                else if (this.transform.GetChild(i).gameObject != charController)
+                {
+                    this.transform.GetChild(i).gameObject.SetActive(false);
+                }
+            }
+        }
+        specialForm.SetActive(true);
+        charController.SetActive(true);
+        specialForm.transform.localScale = specialStartSize;
+        specialForm.transform.localRotation = Quaternion.identity;
+        specialForm.GetComponent<MeshRenderer>().enabled = true;
+        specialForm.GetComponent<MeshCollider>().enabled = true;
+        charController.GetComponent<UserControlMelee>().enabled = false;
+        charController.GetComponent<CharacterMeleeDemo>().enabled = false;
+        charController.GetComponent<CapsuleCollider>().enabled = false;
+        charController.GetComponentInChildren<SkinnedMeshRenderer>().enabled = false;
+        SendMessage("PowerUpActive", true, SendMessageOptions.DontRequireReceiver);
+    }
+
 }
