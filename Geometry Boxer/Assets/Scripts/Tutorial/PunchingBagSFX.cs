d8e25528aad0b4b2bf810e5d27fea3b2a04f1712
Henry Kucab <kucab2345@gmail.com>
2018-01-24 19:09:17 -0700

    Fixed broken prefab

---
+++ Geometry Boxer/Assets/Scripts/Tutorial/PunchingBagSFX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunchingBagSFX : MonoBehaviour {
+
+    private SFX_Manager sfxManager;
+    private AudioSource source;
+    private System.Random rand = new System.Random();
+
+    // Use this for initialization
+    void Start ()
+    {
+        sfxManager = FindObjectOfType<SFX_Manager>();
+        source = this.gameObject.AddComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.transform.root.tag == "Player" && !source.isPlaying && col.impulse.magnitude > 5f)
+        {
+            Debug.Log(col.impulse.magnitude);
+            source.PlayOneShot(sfxManager.lightPunches[rand.Next(0, sfxManager.lightPunches.Count)], col.impulse.magnitude/100f);
+        }
+    }
+}
