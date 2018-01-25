c3036cceea4836aa7573dd38a1bc5774471d92dd
Henry Kucab <kucab2345@gmail.com>
2018-01-24 17:52:54 -0700

    Tutorial Beta finished

---
+++ Geometry Boxer/Assets/Scripts/Tutorial/StopAudioSourceOnExit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopAudioSourceOnExit : MonoBehaviour {
+
+    private AudioSource source;
+    // Use this for initialization
+    void Start()
+    {
+        source = this.GetComponent<AudioSource>();
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if(col.transform.root.tag == "Player")
+        {
+            source.Stop();
+        }
+    }
+}
