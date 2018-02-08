2786238f3bef996ba024775b7a79737ba599f5a2
junok <u0830932@utah.edu>
2018-02-07 19:20:01 -0700

    Got Volume Control working
    Only implemented on Cube Shipping Yard atm.  Added new things to SFX_Manager and PauseMenu prefabs and added the Option Menu Prefab

---
+++ Geometry Boxer/Assets/VolumeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeController : MonoBehaviour {
+
+    public Slider VolumeSlider;
+    private AudioSource[] audios;
+	// Use this for initialization
+	void Start () {
+        audios = this.gameObject.GetComponents<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach(AudioSource a in audios)
+        {
+            a.volume = VolumeSlider.value;
+        }
+	}
+}
