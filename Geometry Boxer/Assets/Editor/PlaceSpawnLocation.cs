0b47f896745b03f8bbbee459c5b0190ceb9ccd48
Chandler Waugh <chandler@gbpackerfans.com>
2018-04-02 20:03:36 -0600

    Added root spawn tag. Place spawn tool and select all objects by given tag tools are ready for test.

---
+++ Geometry Boxer/Assets/Editor/PlaceSpawnLocation.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PlaceSpawnLocation : ScriptableWizard
+{
+    [Tooltip("0 is the lowest level, 2 is the highest, always")]
+    [SerializeField]
+    public Levels HeightLevelToSpawnOn = Levels.Low;
+
+    private GameObject spawnPlacer;
+    private GameObject rootSpawn;
+    public enum Levels : int
+    {
+        Low = 0,
+        Medium,
+        High
+    }
+    [MenuItem("Geometry Boxer Tools/Add Spawn Points")]
+
+    static void SelectAllOfTagWizard()
+    {
+        ScriptableWizard.DisplayWizard<PlaceSpawnLocation>("Add Spawn Points", "Finished", "Add Spawn Point");
+    }
+
+    private void Awake()
+    {
+        spawnPlacer = new GameObject("SpawnPlacer");
+        spawnPlacer.AddComponent<BoxCollider>();
+        spawnPlacer.AddComponent<Light>();
+        spawnPlacer.GetComponent<Light>().color = Color.red;
+        spawnPlacer.GetComponent<Light>().intensity = 10f;
+        rootSpawn = GameObject.FindGameObjectWithTag("RootSpawn");
+        if (rootSpawn == null)
+        {
+            rootSpawn = new GameObject("RootSpawn");
+            rootSpawn.tag = "RootSpawn";
+            rootSpawn.AddComponent<RowSpawnFormat>();
+            GameObject level1 = new GameObject("LowLevelSpawn");
+            level1.transform.parent = rootSpawn.transform;
+            GameObject level2 = new GameObject("MediumLevelSpawn");
+            level2.transform.parent = rootSpawn.transform;
+            GameObject level3 = new GameObject("HighLevelSpawn");
+            level3.transform.parent = rootSpawn.transform;
+        }
+    }
+
+    void OnWizardCreate()
+    {
+        //DestroyImmediate(spawnPlacer);
+    }
+
+    private void OnDestroy()
+    {
+        DestroyImmediate(spawnPlacer);
+    }
+
+    private void OnWizardOtherButton()
+    {
+        GameObject spawnToPlace = new GameObject("SpawnPoint");
+        spawnToPlace.AddComponent<BoxCollider>();
+        spawnToPlace.GetComponent<BoxCollider>().isTrigger = true;
+        spawnToPlace.transform.position = spawnPlacer.transform.position;
+        spawnToPlace.transform.parent = rootSpawn.transform.GetChild((int)HeightLevelToSpawnOn);
+    }
+}
---
+++ Geometry Boxer/Assets/Editor/SelectAllObjectOfTag.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SelectAllObjectOfTag : ScriptableWizard
+{
+    public string desiredTag = "Your tag here";
+    [MenuItem("Geometry Boxer Tools/Select All Objects of Tag")]
+    
+    static void SelectAllOfTagWizard()
+    {
+        ScriptableWizard.DisplayWizard<SelectAllObjectOfTag>("Select All Objects of Tag", "Select All", "Select Root Objects Only");
+    }
+
+    void OnWizardCreate()
+    {
+        Selection.objects = GameObject.FindGameObjectsWithTag(desiredTag);
+    }
+
+    private void OnWizardOtherButton()
+    {
+        GameObject[] gameobjects = GameObject.FindGameObjectsWithTag(desiredTag);
+        List<GameObject> rootObjects = new List<GameObject>();
+        foreach(GameObject obj in gameobjects)
+        {
+            if(obj.transform.root == obj.transform)
+            {
+                rootObjects.Add(obj);
+            }
+        }
+        Selection.objects = rootObjects.ToArray();
+    }
+}
