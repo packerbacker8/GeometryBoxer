558c4a77ff70606055cd1c4127815e3095519d78
Henry Kucab <kucab2345@gmail.com>
2018-05-08 13:54:08 -0500

    Fixed inconcistency in audio being preserved between scene changes

--- Geometry Boxer/Assets/Scripts/UI/MusicOptionPauseMenu.cs
+++ Geometry Boxer/Assets/Scripts/UI/MusicOptionPauseMenu.cs
@@ -8,11 +8,12 @@ public class MusicOptionPauseMenu : MonoBehaviour {
     public AudioMixer audioMixerMaster;
     public Slider musicSlider;
 
-    public void Start()
+    public void Awake()
     {
         float currentMusicVolume;
         audioMixerMaster.GetFloat("MasterVolume", out currentMusicVolume);
         musicSlider.value = currentMusicVolume;
+        SetMasterVolume(currentMusicVolume);
     }
 
     public void SetMasterVolume(float volume)
